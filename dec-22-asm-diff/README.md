d985dc99e6acdef0986f6f11464611805560f596
Siddharth Bhat <siddu.druid@gmail.com>
2017-12-23 00:22:24 +0530

    moved repo here

--- dec-22-asm-diff/README.md
+++ dec-22-asm-diff/README.md
@@ -6,4 +6,132 @@ Benchmarking this versus having an actual stack.
 
 - https://gist.github.com/bollu/66a445df8e419735b1dab044b5fd0553
 
+# Sources
+### C source
+```c
+#include <stdio.h>
 
+int ackerman(int m, int n) {
+    if (m == 0) return n + 1;
+    if (m > 0 && n  == 0) return ackerman(m - 1, 1);
+    return ackerman(m - 1, ackerman(m, n - 1));
+}
+
+int main() {
+    printf("%d", ackerman(3, 11));
+    return 0;
+}
+
+```
+
+### Haskell like C Source
+
+# Perf
+### C performance on `-O1`
+```
+╭─bollu@cantordust ~/work/sxhc/haskell-microbenchmarks/ackerman/c  ‹master*› 
+╰─$ sudo perf stat -d ./a.out                   
+[sudo] password for bollu: 
+16381
+ Performance counter stats for './a.out':
+
+        216.792392      task-clock (msec)         #    0.998 CPUs utilized          
+                 1      context-switches          #    0.005 K/sec                  
+                 0      cpu-migrations            #    0.000 K/sec                  
+               110      page-faults               #    0.507 K/sec                  
+      64,76,73,623      cycles                    #    2.988 GHz                      (48.41%)
+    1,78,49,43,817      instructions              #    2.76  insn per cycle           (61.32%)
+      61,55,63,749      branches                  # 2839.416 M/sec                    (61.27%)
+          4,01,246      branch-misses             #    0.07% of all branches          (62.82%)
+      17,89,24,775      L1-dcache-loads           #  825.328 M/sec                    (60.61%)
+       3,29,16,161      L1-dcache-load-misses     #   18.40% of all L1-dcache hits    (26.02%)
+            63,958      LLC-loads                 #    0.295 M/sec                    (24.75%)
+             5,780      LLC-load-misses           #    9.04% of all LL-cache hits     (35.81%)
+
+       0.217306155 seconds time elapsed
+```
+
+### Haskell like C performance on `-O1`
+
+# Asm
+
+### C
+```asm
+	.text
+	.file	"test.c"
+	.globl	ackerman                # -- Begin function ackerman
+	.p2align	4, 0x90
+	.type	ackerman,@function
+ackerman:                               # @ackerman
+	.cfi_startproc
+# %bb.0:
+	pushq	%rbx
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbx, -16
+                                        # kill: def %edi killed %edi def %rdi
+	testl	%edi, %edi
+	je	.LBB0_1
+	.p2align	4, 0x90
+.LBB0_3:                                # =>This Inner Loop Header: Depth=1
+	leal	-1(%rdi), %ebx
+	testl	%edi, %edi
+	jle	.LBB0_6
+# %bb.4:                                #   in Loop: Header=BB0_3 Depth=1
+	movl	$1, %eax
+	testl	%esi, %esi
+	je	.LBB0_5
+.LBB0_6:                                #   in Loop: Header=BB0_3 Depth=1
+	addl	$-1, %esi
+                                        # kill: def %edi killed %edi killed %rdi
+	callq	ackerman
+.LBB0_5:                                #   in Loop: Header=BB0_3 Depth=1
+	movl	%eax, %esi
+	movl	%ebx, %edi
+	testl	%ebx, %ebx
+	jne	.LBB0_3
+	jmp	.LBB0_2
+.LBB0_1:
+	movl	%esi, %eax
+.LBB0_2:
+	addl	$1, %eax
+	popq	%rbx
+	retq
+.Lfunc_end0:
+	.size	ackerman, .Lfunc_end0-ackerman
+	.cfi_endproc
+                                        # -- End function
+	.globl	main                    # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	pushq	%rax
+	.cfi_def_cfa_offset 16
+	movl	$3, %edi
+	movl	$11, %esi
+	callq	ackerman
+	movl	%eax, %ecx
+	movl	$.L.str, %edi
+	xorl	%eax, %eax
+	movl	%ecx, %esi
+	callq	printf
+	xorl	%eax, %eax
+	popq	%rcx
+	retq
+.Lfunc_end1:
+	.size	main, .Lfunc_end1-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L.str,@object          # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"%d"
+	.size	.L.str, 3
+
+
+	.ident	"clang version 6.0.0 (https://github.com/llvm-mirror/clang.git 40e9a74cba88c271af3407dad30006386881097b) (https://github.com/llvm-mirror/llvm.git 981877461a4a4387994841065d35f4897fe8dfeb)"
+	.section	".note.GNU-stack","",@progbits
+
+```
+### Haskell like C
--- dec-22-asm-diff/haskell-like-c.s
+++ dec-22-asm-diff/haskell-like-c.s
@@ -6,25 +6,14 @@
 pushReturn:                             # @pushReturn
 	.cfi_startproc
 # %bb.0:
-	movslq	g_ret_sp(%rip), %rax
-	cmpq	$1000000, %rax          # imm = 0xF4240
-	jae	.LBB0_2
-# %bb.1:
-	movq	g_ret_stack(%rip), %rcx
-	leal	1(%rax), %edx
+	movq	g_ret_stack(%rip), %rax
+	movslq	g_ret_sp(%rip), %rcx
+	leal	1(%rcx), %edx
 	movl	%edx, g_ret_sp(%rip)
-	shlq	$4, %rax
-	movq	%rdi, (%rcx,%rax)
-	movq	%rsi, 8(%rcx,%rax)
+	shlq	$4, %rcx
+	movq	%rdi, (%rax,%rcx)
+	movq	%rsi, 8(%rax,%rcx)
 	retq
-.LBB0_2:
-	pushq	%rax
-	.cfi_def_cfa_offset 16
-	movl	$.L.str, %edi
-	movl	$.L.str.1, %esi
-	movl	$47, %edx
-	movl	$.L__PRETTY_FUNCTION__.pushReturn, %ecx
-	callq	__assert_fail
 .Lfunc_end0:
 	.size	pushReturn, .Lfunc_end0-pushReturn
 	.cfi_endproc
@@ -35,25 +24,14 @@ pushReturn:                             # @pushReturn
 popReturn:                              # @popReturn
 	.cfi_startproc
 # %bb.0:
-	movq	g_ret_stack(%rip), %rcx
-	movslq	g_ret_sp(%rip), %rax
-	leaq	-1(%rax), %rdx
-	movl	%edx, g_ret_sp(%rip)
-	testq	%rax, %rax
-	jle	.LBB1_2
-# %bb.1:
-	shlq	$4, %rdx
-	movq	(%rcx,%rdx), %rax
-	movq	8(%rcx,%rdx), %rdx
+	movslq	g_ret_sp(%rip), %rcx
+	addq	$-1, %rcx
+	movl	%ecx, g_ret_sp(%rip)
+	movq	g_ret_stack(%rip), %rdx
+	shlq	$4, %rcx
+	movq	(%rdx,%rcx), %rax
+	movq	8(%rdx,%rcx), %rdx
 	retq
-.LBB1_2:
-	pushq	%rax
-	.cfi_def_cfa_offset 16
-	movl	$.L.str.2, %edi
-	movl	$.L.str.1, %esi
-	movl	$54, %edx
-	movl	$.L__PRETTY_FUNCTION__.popReturn, %ecx
-	callq	__assert_fail
 .Lfunc_end1:
 	.size	popReturn, .Lfunc_end1-popReturn
 	.cfi_endproc
@@ -90,7 +68,7 @@ mkclosure1:                             # @mkclosure1
 main_return:                            # @main_return
 	.cfi_startproc
 # %bb.0:
-	movl	$.L.str.3, %edi
+	movl	$.L.str, %edi
 	xorl	%eax, %eax
 	movl	%edx, %esi
 	jmp	printf                  # TAILCALL
@@ -117,66 +95,51 @@ case_ackerman_aval_bdec:                # @case_ackerman_aval_bdec
 ackerman:                               # @ackerman
 	.cfi_startproc
 # %bb.0:
-	pushq	%rax
+	pushq	%rbp
 	.cfi_def_cfa_offset 16
-	movl	%esi, %eax
-	testl	%edi, %edi
-	je	.LBB6_4
-# %bb.1:
-	movl	%edi, %r8d
+	pushq	%rbx
+	.cfi_def_cfa_offset 24
+	pushq	%rax
+	.cfi_def_cfa_offset 32
+	.cfi_offset %rbx, -24
+	.cfi_offset %rbp, -16
+	movl	%esi, %ebx
+	movl	%edi, %ebp
+	testl	%ebp, %ebp
+	jne	.LBB6_2
+	jmp	.LBB6_4
 	.p2align	4, 0x90
+.LBB6_3:                                #   in Loop: Header=BB6_2 Depth=1
+	movl	$1, %ebx
+	testl	%ebp, %ebp
+	je	.LBB6_4
 .LBB6_2:                                # =>This Loop Header: Depth=1
-                                        #     Child Loop BB6_6 Depth 2
-	leal	-1(%r8), %edx
-	testl	%eax, %eax
+                                        #     Child Loop BB6_5 Depth 2
+	addl	$-1, %ebp
+	testl	%ebx, %ebx
 	je	.LBB6_3
 	.p2align	4, 0x90
-.LBB6_6:                                #   Parent Loop BB6_2 Depth=1
+.LBB6_5:                                #   Parent Loop BB6_2 Depth=1
                                         # =>  This Inner Loop Header: Depth=2
-	movslq	g_ret_sp(%rip), %rsi
-	cmpq	$1000000, %rsi          # imm = 0xF4240
-	jae	.LBB6_9
-# %bb.7:                                #   in Loop: Header=BB6_6 Depth=2
-	movq	g_ret_stack(%rip), %rdi
-	leal	1(%rsi), %ecx
-	movl	%ecx, g_ret_sp(%rip)
-	shlq	$4, %rsi
-	movq	$case_ackerman_aval_bdec, (%rdi,%rsi)
-	movq	%rdx, 8(%rdi,%rsi)
-	addl	$-1, %eax
-	jne	.LBB6_6
-.LBB6_3:                                #   in Loop: Header=BB6_2 Depth=1
-	addq	$-1, %r8
-	movl	$1, %eax
-	testl	%edx, %edx
-	jne	.LBB6_2
+	movl	$case_ackerman_aval_bdec, %edi
+	movl	%ebp, %esi
+	callq	mkclosure1
+	movq	%rax, %rdi
+	movq	%rdx, %rsi
+	callq	pushReturn
+	addl	$-1, %ebx
+	jne	.LBB6_5
+	jmp	.LBB6_3
 .LBB6_4:
-	movq	g_ret_stack(%rip), %rcx
-	movslq	g_ret_sp(%rip), %rsi
-	leaq	-1(%rsi), %rdx
-	movl	%edx, g_ret_sp(%rip)
-	testq	%rsi, %rsi
-	jle	.LBB6_8
-# %bb.5:
-	shlq	$4, %rdx
-	movq	(%rcx,%rdx), %rdi
-	movq	8(%rcx,%rdx), %rsi
-	addl	$1, %eax
-	movl	%eax, %edx
-	popq	%rax
-	jmpq	*%rdi                   # TAILCALL
-.LBB6_9:
-	movl	$.L.str, %edi
-	movl	$.L.str.1, %esi
-	movl	$47, %edx
-	movl	$.L__PRETTY_FUNCTION__.pushReturn, %ecx
-	callq	__assert_fail
-.LBB6_8:
-	movl	$.L.str.2, %edi
-	movl	$.L.str.1, %esi
-	movl	$54, %edx
-	movl	$.L__PRETTY_FUNCTION__.popReturn, %ecx
-	callq	__assert_fail
+	callq	popReturn
+	addl	$1, %ebx
+	movq	%rax, %rdi
+	movq	%rdx, %rsi
+	movl	%ebx, %edx
+	addq	$8, %rsp
+	popq	%rbx
+	popq	%rbp
+	jmpq	*%rax                   # TAILCALL
 .Lfunc_end6:
 	.size	ackerman, .Lfunc_end6-ackerman
 	.cfi_endproc
@@ -192,154 +155,17 @@ main:                                   # @main
 	movl	$16000000, %edi         # imm = 0xF42400
 	callq	malloc
 	movq	%rax, g_ret_stack(%rip)
-	movslq	g_ret_sp(%rip), %rcx
-	cmpq	$1000000, %rcx          # imm = 0xF4240
-	jae	.LBB7_15
-# %bb.1:
-	leaq	1(%rcx), %rdx
-	movl	%edx, g_ret_sp(%rip)
-	movq	%rcx, %rsi
-	shlq	$4, %rsi
-	movq	$main_return, (%rax,%rsi)
-	movabsq	$223338299444, %rdi     # imm = 0x3400000034
-	movq	%rdi, 8(%rax,%rsi)
-	cmpl	$1000000, %edx          # imm = 0xF4240
-	jae	.LBB7_15
-# %bb.2:
-	movq	g_ret_stack(%rip), %rsi
-	leaq	2(%rcx), %rax
-	movl	%eax, g_ret_sp(%rip)
-	shlq	$4, %rdx
-	movq	$case_ackerman_aval_bdec, (%rsi,%rdx)
-	movq	$2, 8(%rsi,%rdx)
-	cmpl	$1000000, %eax          # imm = 0xF4240
-	jae	.LBB7_15
-# %bb.3:
-	movq	g_ret_stack(%rip), %rsi
-	leaq	3(%rcx), %rdx
-	movl	%edx, g_ret_sp(%rip)
-	shlq	$4, %rax
-	movq	$case_ackerman_aval_bdec, (%rsi,%rax)
-	movq	$2, 8(%rsi,%rax)
-	cmpl	$999999, %edx           # imm = 0xF423F
-	ja	.LBB7_15
-# %bb.4:
-	movq	g_ret_stack(%rip), %rsi
-	leaq	4(%rcx), %rax
-	movl	%eax, g_ret_sp(%rip)
-	shlq	$4, %rdx
-	movq	$case_ackerman_aval_bdec, (%rsi,%rdx)
-	movq	$2, 8(%rsi,%rdx)
-	cmpl	$999999, %eax           # imm = 0xF423F
-	ja	.LBB7_15
-# %bb.5:
-	movq	g_ret_stack(%rip), %rsi
-	leaq	5(%rcx), %rdx
-	movl	%edx, g_ret_sp(%rip)
-	shlq	$4, %rax
-	movq	$case_ackerman_aval_bdec, (%rsi,%rax)
-	movq	$2, 8(%rsi,%rax)
-	cmpl	$999999, %edx           # imm = 0xF423F
-	ja	.LBB7_15
-# %bb.6:
-	movq	g_ret_stack(%rip), %rsi
-	leaq	6(%rcx), %rax
-	movl	%eax, g_ret_sp(%rip)
-	shlq	$4, %rdx
-	movq	$case_ackerman_aval_bdec, (%rsi,%rdx)
-	movq	$2, 8(%rsi,%rdx)
-	cmpl	$999999, %eax           # imm = 0xF423F
-	ja	.LBB7_15
-# %bb.7:
-	movq	g_ret_stack(%rip), %rsi
-	leal	7(%rcx), %edx
-	movl	%edx, g_ret_sp(%rip)
-	shlq	$4, %rax
-	movq	$case_ackerman_aval_bdec, (%rsi,%rax)
-	movq	$2, 8(%rsi,%rax)
-	cmpl	$999999, %edx           # imm = 0xF423F
-	ja	.LBB7_15
-# %bb.8:
-	movslq	%edx, %rdx
-	movq	g_ret_stack(%rip), %rsi
-	leal	8(%rcx), %eax
-	movl	%eax, g_ret_sp(%rip)
-	shlq	$4, %rdx
-	movq	$case_ackerman_aval_bdec, (%rsi,%rdx)
-	movq	$2, 8(%rsi,%rdx)
-	cmpl	$999999, %eax           # imm = 0xF423F
-	ja	.LBB7_15
-# %bb.9:
-	movslq	%eax, %rdx
-	movq	g_ret_stack(%rip), %rsi
-	leal	9(%rcx), %eax
-	movl	%eax, g_ret_sp(%rip)
-	shlq	$4, %rdx
-	movq	$case_ackerman_aval_bdec, (%rsi,%rdx)
-	movq	$2, 8(%rsi,%rdx)
-	cmpl	$999999, %eax           # imm = 0xF423F
-	ja	.LBB7_15
-# %bb.10:
-	movslq	%eax, %rdx
-	movq	g_ret_stack(%rip), %rsi
-	leal	10(%rcx), %eax
-	movl	%eax, g_ret_sp(%rip)
-	shlq	$4, %rdx
-	movq	$case_ackerman_aval_bdec, (%rsi,%rdx)
-	movq	$2, 8(%rsi,%rdx)
-	cmpl	$999999, %eax           # imm = 0xF423F
-	ja	.LBB7_15
-# %bb.11:
-	movslq	%eax, %rdx
-	movq	g_ret_stack(%rip), %rsi
-	leal	11(%rcx), %eax
-	movl	%eax, g_ret_sp(%rip)
-	shlq	$4, %rdx
-	movq	$case_ackerman_aval_bdec, (%rsi,%rdx)
-	movq	$2, 8(%rsi,%rdx)
-	cmpl	$999999, %eax           # imm = 0xF423F
-	ja	.LBB7_15
-# %bb.12:
-	movslq	%eax, %rdx
-	movq	g_ret_stack(%rip), %rsi
-	leal	12(%rcx), %eax
-	movl	%eax, g_ret_sp(%rip)
-	shlq	$4, %rdx
-	movq	$case_ackerman_aval_bdec, (%rsi,%rdx)
-	movq	$2, 8(%rsi,%rdx)
-	cmpl	$1000000, %eax          # imm = 0xF4240
-	jae	.LBB7_15
-# %bb.13:
-	cltq
-	movq	g_ret_stack(%rip), %rdx
-	addl	$13, %ecx
-	movl	%ecx, g_ret_sp(%rip)
-	shlq	$4, %rax
-	movq	$case_ackerman_aval_bdec, (%rdx,%rax)
-	movq	$1, 8(%rdx,%rax)
-	cmpl	$1000000, %ecx          # imm = 0xF4240
-	jae	.LBB7_15
-# %bb.14:
-	movslq	%ecx, %rax
-	movq	g_ret_stack(%rip), %rdx
-	shlq	$4, %rax
-	movq	$case_ackerman_aval_bdec, (%rdx,%rax)
-	movq	$0, 8(%rdx,%rax)
-	movq	g_ret_stack(%rip), %rdx
-	movl	%ecx, g_ret_sp(%rip)
-	movq	(%rdx,%rax), %rdi
-	movq	8(%rdx,%rax), %rsi
-	movl	$2, %edx
-	callq	*%rdi
+	movl	$main_return, %edi
+	callq	mkclosure0
+	movabsq	$223338299444, %rsi     # imm = 0x3400000034
+	movq	%rax, %rdi
+	callq	pushReturn
+	movl	$3, %edi
+	movl	$11, %esi
+	callq	ackerman
 	xorl	%eax, %eax
 	popq	%rcx
 	retq
-.LBB7_15:
-	movl	$.L.str, %edi
-	movl	$.L.str.1, %esi
-	movl	$47, %edx
-	movl	$.L__PRETTY_FUNCTION__.pushReturn, %ecx
-	callq	__assert_fail
 .Lfunc_end7:
 	.size	main, .Lfunc_end7-main
 	.cfi_endproc
@@ -352,38 +178,13 @@ g_ret_sp:
 	.long	0                       # 0x0
 	.size	g_ret_sp, 4
 
+	.type	g_ret_stack,@object     # @g_ret_stack
+	.comm	g_ret_stack,8,8
 	.type	.L.str,@object          # @.str
 	.section	.rodata.str1.1,"aMS",@progbits,1
 .L.str:
-	.asciz	"g_ret_sp <= STACK_SIZE - 1"
-	.size	.L.str, 27
-
-	.type	.L.str.1,@object        # @.str.1
-.L.str.1:
-	.asciz	"test.c"
-	.size	.L.str.1, 7
-
-	.type	.L__PRETTY_FUNCTION__.pushReturn,@object # @__PRETTY_FUNCTION__.pushReturn
-.L__PRETTY_FUNCTION__.pushReturn:
-	.asciz	"void pushReturn(Closure)"
-	.size	.L__PRETTY_FUNCTION__.pushReturn, 25
-
-	.type	g_ret_stack,@object     # @g_ret_stack
-	.comm	g_ret_stack,8,8
-	.type	.L.str.2,@object        # @.str.2
-.L.str.2:
-	.asciz	"g_ret_sp >= 0"
-	.size	.L.str.2, 14
-
-	.type	.L__PRETTY_FUNCTION__.popReturn,@object # @__PRETTY_FUNCTION__.popReturn
-.L__PRETTY_FUNCTION__.popReturn:
-	.asciz	"Closure popReturn()"
-	.size	.L__PRETTY_FUNCTION__.popReturn, 20
-
-	.type	.L.str.3,@object        # @.str.3
-.L.str.3:
 	.asciz	"%d"
-	.size	.L.str.3, 3
+	.size	.L.str, 3
 
 
 	.ident	"clang version 6.0.0 (https://github.com/llvm-mirror/clang.git 40e9a74cba88c271af3407dad30006386881097b) (https://github.com/llvm-mirror/llvm.git 981877461a4a4387994841065d35f4897fe8dfeb)"
